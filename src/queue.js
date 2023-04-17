5835f3773284abc18d852ebc54a849f9fe87f6ee
Inari13066 <devil13066@gmail.com>
2023-04-17 22:05:21 +0300

    feat: make queue

--- src/queue.js
+++ src/queue.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 // const { ListNode } = require('../extensions/list-node.js');
 
@@ -13,24 +13,42 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
+
+class ListNode {
+    constructor(x) {
+        this.value = x;
+        this.next = null;
+    }
+}
+
 class Queue {
+    constructor() {
+        this.root = null;
+    }
 
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    getUnderlyingList() {
+        return this.root;
+    }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    enqueue(value) {
+        if (this.root == null) {
+            this.root = new ListNode(value);
+        } else {
+            let tmp = this.root;
+            while (tmp.next != null) {
+                tmp = tmp.next;
+            }
+            tmp.next = new ListNode(value);
+        }
+    }
 
-  dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    dequeue() {
+        let tmp = this.root;
+        this.root = tmp.next;
+        return tmp.value;
+    }
 }
 
 module.exports = {
-  Queue
+    Queue,
 };
